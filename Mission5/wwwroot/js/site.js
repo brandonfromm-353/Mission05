faec4eec0029bc525ac6dc0c01b4f31fdca6f862
brandonfromm <156289370+brandonfromm@users.noreply.github.com>
2025-02-05 21:32:30 -0700

    fixed icon tab and added comments

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -1,14 +1,16 @@
-﻿ $('#btnSubmit').click(function() {
-        event.preventDefault()
-        let hours = $('#Hours').val()
-        const rate = 10
-        if (hours > 0) {
-            let total = hours * rate
-            $('#Total').val('$' + total.toFixed(2))
-        } else if (hours == 0) {
-            alert('Please enter a valid number')
-        } else {
-            alert('Please enter a positive number of hours.')
-        }
-    })
+﻿// jQuery for the calculator
+$('#btnSubmit').click(function() {
+    event.preventDefault()
+    let hours = $('#Hours').val()
+    const rate = 10
+    // Makes sure the users only input a positive number above 0
+    if (hours > 0) {
+        let total = hours * rate
+        $('#Total').val('$' + total.toFixed(2))
+    } else if (hours == 0) {
+        alert('Please enter a valid number')
+    } else {
+        alert('Please enter a positive number of hours.')
+    }
+})
 
