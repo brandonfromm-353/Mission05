d2622fae39600f2957373d628764bac5e4588cf5
brandonfromm <156289370+brandonfromm@users.noreply.github.com>
2025-02-05 19:42:51 -0700

    Initial commit

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿ $('#btnSubmit').click(function() {
+        event.preventDefault()
+        let hours = $('#Hours').val()
+        const rate = 10
+        if (hours > 0) {
+            let total = hours * rate
+            $('#Total').val('$' + total.toFixed(2))
+        } else if (hours == 0) {
+            alert('Please enter a valid number')
+        } else {
+            alert('Please enter a positive number of hours.')
+        }
+    })
+
